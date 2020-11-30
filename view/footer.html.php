f1779d4d7e7d5315f75a537643ab0e2d50506044
lambertj <dev.lambertj@gmail.com>
2020-11-30 12:27:12 +0100

    Ajouts des bibliothèques bootstrap et inclusion dans header/footer.

--- view/footer.html.php
+++ view/footer.html.php
@@ -1,6 +1,9 @@
 </main>
 
 <footer>
+    <script src="/asset/lib/jquery/jquery.min.js"></script>
+    <script src="/asset/lib/popper/popper.min.js"></script>
+    <script src="/asset/lib/bootstrap/bootstrap.min.js"></script>
 </footer>
 </body>
 
--- view/header.html.php
+++ view/header.html.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Mon Blog - Accueil</title>
+    <link rel="stylesheet" href="/asset/lib/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="styles.css" />
 </head>
 
