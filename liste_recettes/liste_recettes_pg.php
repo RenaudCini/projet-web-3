39e200eeda1524b3d4d5ad00ed43e4a6e6604678
7ridzse <fridzse.jarmasson@laposte.net>
2020-11-30 16:25:12 +0100

    création des cards

--- liste_recettes/liste_recettes_pg.php
+++ liste_recettes/liste_recettes_pg.php
@@ -10,10 +10,17 @@
 
 <body>
 
-    <p>Hello world !</p>
-
-    <button type="button" class="btn btn-primary">Primary</button>
-
+    <div class="row">
+        <div class="col-md-4 pb-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="text-right small text-secondary">Ecrit par USERNAME le DATE</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <footer>
         <script src="/asset/lib/jquery/jquery.min.js"></script>
