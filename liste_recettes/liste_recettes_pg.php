c977c2cebe074fefb7245b754213d3c5fffd780a
lambertj <dev.lambertj@gmail.com>
2020-11-30 15:23:35 +0100

    Inclusion des fichiers bootstrap.

---
+++ liste_recettes/liste_recettes_pg.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/asset/lib/bootstrap/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <p>Hello world !</p>
+
+    <button type="button" class="btn btn-primary">Primary</button>
+
+
+    <footer>
+        <script src="/asset/lib/jquery/jquery.min.js"></script>
+        <script src="/asset/lib/popper/popper.min.js"></script>
+        <script src="/asset/lib/bootstrap/bootstrap.min.js"></script>
+    </footer>
+
+</body>
+
+</html>
