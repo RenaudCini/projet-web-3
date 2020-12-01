e6b2a5727f57094f00a94b52a92fe2eee21788c8
lambertj <dev.lambertj@gmail.com>
2020-12-01 13:44:57 +0100

    Finition interface front et ajout JS

---
+++ asset/js/creation_recette.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+
+    // Ajout d'un input ingrédient :
+    $('.btnAddIngredient').click(function () {
+        let inputIngredient = $('.inputIngredientModele').html();
+        $('.ingredients').append(inputIngredient);
+        $('.ingredients > .ingredient:last-child .btnSuppr').removeClass('d-none');
+    });
+
+    // Ajout d'un input étape :
+    $('.btnAddEtape').click(function () {
+        let inputEtape = $('.inputEtapeModele').html();
+        $('.etapes').append(inputEtape);
+        $('.etapes > .etape:last-child .btnSuppr').removeClass('d-none').attr('typeInput', 'etape');
+        let maxEtape = recupMaxEtape();
+        let newEtape = maxEtape + 1;
+        $('.etapes > .etape:last-child .etapeContenu').attr('noEtape', newEtape);
+        $('.etapes > .etape:last-child .badge').html("&nbsp;" + newEtape + "&nbsp;");
+    });
+
+    // Suppression d'un input :
+    $('body').on('click', '.btnSuppr', function () {
+        $(this).parent().parent().remove();
+        if ($(this).attr('typeInput') === 'etape') {
+            remplacerEtapes();
+        }
+    });
+
+    // Envoi du formulaire :
+    $('.btnEnvoiFormulaire').click(function () {
+
+        let titre = $('#titre').val();
+        let budget = $('#budget').val();
+        let difficulte = $('#difficulte').val();
+        let temps = $('#temps').val();
+        let image = $('#image').val();
+
+
+        let reponsesFormulaire = {
+            titre,
+            ingredients: [],
+            etapes: [],
+            budget,
+            difficulte,
+            temps,
+            image
+        };
+
+        // Ingrédients :
+        $.each($('.ingredient'), function () {
+            let nom = $(this).find('.ingredientNom').val();
+            let quantite = $(this).find('.quantite').val();
+            let mesure = $(this).find('.mesure option:selected').val();
+
+            let jsonIngredient = {
+                "nom": nom,
+                "quantite": quantite,
+                "mesure": mesure
+            }
+            reponsesFormulaire.ingredients.push(jsonIngredient);
+        });
+
+        // Etapes :
+        $.each($('.etapeContenu'), function () {
+            let noEtape = parseInt($(this).attr('noEtape'));
+            let contenu = $(this).val();
+
+            let jsonEtape = {
+                "etape": noEtape,
+                "contenu": contenu
+            }
+            reponsesFormulaire.etapes.push(jsonEtape);
+        });
+
+        console.log(reponsesFormulaire);
+    });
+
+    /**
+     * Permet de récupérer le numéro d'étape maximum pour pouvoir créer une nouvelle étape qui sera ce nombre + 1.
+     */
+    function recupMaxEtape() {
+        let etapes = [];
+        $.each($('.etapeContenu'), function () {
+            etapes.push(parseInt($(this).attr('noEtape')));
+        });
+        return Math.max.apply(null, etapes);
+    }
+
+    /**
+     * Permet de remplacer les numéros d'étapes par les bons numéros en cas de suppression d'une étape.
+     */
+    function remplacerEtapes() {
+        let i = 1;
+        $.each($('.etape'), function () {
+            $(this).find('.etapeContenu').attr('noEtape', i);
+            $(this).find('.badge').html("&nbsp;" + i + "&nbsp;");
+            i++;
+        });
+    }
+
+});
--- creation_recette/creation_recette_pg.php
+++ creation_recette/creation_recette_pg.php
@@ -18,68 +18,129 @@
 
         <div class="text-center">
 
+            <!-- Titre -->
+            <div class="offset-md-2 col-md-8">
+                <input type="text" id="titre" class="text-center form-control" placeholder="Titre de la recette">
+            </div>
+
+            <br>
+
             <!-- Ingrédients -->
-            <div class="card ingredients">
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Ingrédients</h5>
 
                     <br>
 
-                    <div class="offset-md-2 col-md-8">
-                        <form>
-                            <div class="form-row">
-                                <div class="col">
-                                    <input type="text" class="text-center form-control ingredient" placeholder="Ingrédient">
-                                </div>
-                                <div class="col">
-                                    <input type="text" class="text-center form-control quantite" placeholder="Quantité">
-                                </div>
-                                <div class="col">
-                                    <div class="form-group">
-                                        <select class="text-center form-control mesure">
-                                            <option value='null'>Unité</option>
-                                            <?php
-                                            $bdd = new creation_recette_sc;
-                                            $mesures = $bdd->listeMesures();
-                                            foreach ($mesures as $mesure) : ?>
-                                                <option value='<?= $mesure['id'] ?>'><?= $mesure['nom'] ?></option>
-                                            <?php endforeach; ?>
-                                        </select>
+                    <div class="ingredients">
+                        <div class="inputIngredientModele">
+                            <div class="ingredient row">
+                                <div class="offset-md-2 col-md-8">
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <input type="text" class="text-center form-control ingredientNom" placeholder="Ingrédient">
+                                        </div>
+                                        <div class="col">
+                                            <input type="text" class="text-center form-control quantite" placeholder="Quantité">
+                                        </div>
+                                        <div class="col">
+                                            <div class="form-group">
+                                                <select class="text-center form-control mesure">
+                                                    <option value='null'>Unité</option>
+                                                    <?php
+                                                    $bdd = new creation_recette_sc;
+                                                    $mesures = $bdd->listeMesures();
+                                                    foreach ($mesures as $mesure) : ?>
+                                                        <option value='<?= $mesure['id'] ?>'><?= $mesure['nom'] ?></option>
+                                                    <?php endforeach; ?>
+                                                </select>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                                <div class="col-md-2 text-left"><button class="btn btn-danger btnSuppr d-none">X</button></div>
                             </div>
-                        </form>
+                        </div>
                     </div>
 
                     <br>
 
-                    <div class="text-center"><button class="btn btn-primary">Ajouter un ingrédient</button></div>
+                    <div class="text-center"><button class="btn btn-primary btnAddIngredient">Ajouter un ingrédient</button></div>
                 </div>
             </div>
 
             <br>
 
+            <!-- Préparation -->
             <div class="card preparation">
                 <div class="card-body">
-                    This is some text within a card body.
+                    <h5 class="card-title">Préparation</h5>
+
+                    <br>
+
+                    <div class="etapes">
+                        <div class="inputEtapeModele">
+                            <div class="etape row">
+                                <div class="offset-md-1 col-md-1 text-right">
+                                    <h3><span class="badge badge-dark p-2">&nbsp;1&nbsp;</span></h3>
+                                </div>
+                                <div class="form-group col-md-8">
+                                    <textarea class="form-control etapeContenu" noEtape="1" rows="3"></textarea>
+                                </div>
+                                <div class="col-md-2 text-left"><button class="btn btn-danger btnSuppr d-none">X</button></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <div class="text-center"><button class="btn btn-primary btnAddEtape">Ajouter une étape</button></div>
+
                 </div>
             </div>
+
             <br>
+
+            <!-- Informations (difficulté, budget, temps) -->
             <div class="card informations">
                 <div class="card-body">
-                    This is some text within a card body.
+                    <form class="offset-md-2 col-md-8">
+                        <div class="form-group row">
+                            <label for="budget" class="text-right col-sm-2 col-form-label">Budget</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="budget">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="difficulte" class="text-right col-sm-2 col-form-label">Difficulté</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="difficulte">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="temps" class="text-right col-sm-2 col-form-label">Temps</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="temps">
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+
             <br>
-            <div class="card image">
-                <div class="card-body">
-                    This is some text within a card body.
-                </div>
+
+            <!-- Image -->
+            <div class="offset-md-2 col-md-8">
+                <input type="text" id="image" class="text-center form-control" placeholder="URL de l'image">
             </div>
+
+            <br>
+
+            <button class="btn btn-success btnEnvoiFormulaire">Valider</button>
+
+            <br><br>
+
         </div>
-
-
-
 
     </div>
 
@@ -87,6 +148,7 @@
         <script src="/asset/lib/jquery/jquery.min.js"></script>
         <script src="/asset/lib/popper/popper.min.js"></script>
         <script src="/asset/lib/bootstrap/bootstrap.min.js"></script>
+        <script src="/asset/js/creation_recette.js"></script>
     </footer>
 
 </body>
