f601581178c57b5ba97d3a09d66fc7b1aa9399ab
lambertj <dev.lambertj@gmail.com>
2020-12-01 15:17:31 +0100

    Finition du traitement AJAX pour ajouter une recette.

--- creation_recette/creation_recette_tr.php
+++ creation_recette/creation_recette_tr.php
@@ -2,23 +2,73 @@
 
 require_once 'creation_recette_sc.php';
 
-if (filter_input(INPUT_POST, 'creationRecette') && filter_input(INPUT_POST, 'reponsesFormulaire')) {
+if ($_POST['creationRecette'] && $_POST['reponsesFormulaire']) {
 
     $bdd = new BDD;
-    $bdd->insertDonne(
+
+    $reponses = $_POST['reponsesFormulaire'];
+
+    // Insertion dans la table recettes :
+    $idRecette = $bdd->insertDonne(
         'recettes',
         'titre, difficulte, budget, temps, date, image, id_utilisateurs',
-        ':titre, :difficulte, :budget, :temps, :date, :image, :id_utilisateurs',
+        ':titre, :difficulte, :budget, :temps, NOW(), :image, :id_utilisateurs',
         [
-            'titre' => 'Titre test',
-            'difficulte' => '3',
-            'budget' => '4',
-            'temps' => '50 min',
-            'date' => 'NOW()',
-            'image' => 'test.com',
+            'titre' => $reponses['titre'],
+            'difficulte' => $reponses['difficulte'],
+            'budget' => $reponses['budget'],
+            'temps' => $reponses['temps'],
+            'image' => $reponses['image'],
             'id_utilisateurs' => '2'
         ]
     );
 
-    echo json_encode(utf8_encode("La recette a bien été créée !"));
+    // Insertion des étapes :
+    $etapes = $reponses['etapes'];
+    foreach ($etapes as $etape) {
+        $bdd->insertDonne(
+            'etapes',
+            'no_etape, contenu, id_recettes',
+            ':no_etape, :contenu, :id_recettes',
+            [
+                'no_etape' => $etape['etape'],
+                'contenu' => $etape['contenu'],
+                'id_recettes' => $idRecette
+            ]
+        );
+    }
+
+    // Insertion de la composition :
+    $ingredients = $reponses['ingredients'];
+    foreach ($ingredients as $ingredient) {
+        // On regarde si l'ingrédient existe déjà, si oui on récupère son ID, si non on l'insère :
+        $select = 'SELECT id FROM ingredients WHERE nom = :nom';
+        $resultat = $bdd->selectUneDonne($select, ['nom' => $ingredient['nom']]);
+        if ($resultat) {
+            $idIngredient = $resultat['id'];
+        } else {
+            $idIngredient = $bdd->insertDonne(
+                'ingredients',
+                'nom',
+                ':nom',
+                ['nom' => $ingredient['nom']]
+            );
+        }
+
+        // On insère enfin la composition :
+        $bdd->insertDonne(
+            'compositions',
+            'id_recettes, id_ingredients, id_mesures, quantite',
+            ':id_recettes, :id_ingredients, :id_mesures, :quantite',
+            [
+                'id_recettes' => $idRecette,
+                'id_ingredients' => $idIngredient,
+                'id_mesures' => $ingredient['mesure'],
+                'quantite' => $ingredient['quantite'],
+            ]
+        );
+    }
+
+    // Retour AJAX :
+    echo json_encode("La recette a bien été créée !");
 }
