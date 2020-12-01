e45f9ab812361c473de2fa86842c6cd80b6cf724
lambertj <dev.lambertj@gmail.com>
2020-12-01 14:41:04 +0100

    Utilisation de la requête d'insertion

--- creation_recette/creation_recette_tr.php
+++ creation_recette/creation_recette_tr.php
@@ -4,7 +4,21 @@ require_once 'creation_recette_sc.php';
 
 if (filter_input(INPUT_POST, 'creationRecette') && filter_input(INPUT_POST, 'reponsesFormulaire')) {
 
-    $bdd = new creation_recette_sc;
+    $bdd = new BDD;
+    $bdd->insertDonne(
+        'recettes',
+        'titre, difficulte, budget, temps, date, image, id_utilisateurs',
+        ':titre, :difficulte, :budget, :temps, :date, :image, :id_utilisateurs',
+        [
+            'titre' => 'Titre test',
+            'difficulte' => '3',
+            'budget' => '4',
+            'temps' => '50 min',
+            'date' => 'NOW()',
+            'image' => 'test.com',
+            'id_utilisateurs' => '2'
+        ]
+    );
 
     echo json_encode(utf8_encode("La recette a bien été créée !"));
 }
