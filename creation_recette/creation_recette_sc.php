3e31ffc49ac9ab7fca058b739a536512e8197c85
RenaudCini <renaudcini@gmail.com>
2020-12-01 09:45:32 +0100

    ajout classe bdd fonction listMesure

---
+++ creation_recette/creation_recette_sc.php
@@ -0,0 +1,83 @@
+<?php
+
+
+class creation_recette_sc extends model
+{
+
+    /**
+     *
+     */
+    public function listeMesures()
+    {
+        return $this->db->selectTouteDonne("SELECT * FROM mesures");
+    }
+
+
+}
+
+class model
+{
+    /**
+     * model constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new \BDD;
+    }
+
+}
+
+
+class BDD
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new PDO("mysql:host=localhost;dbname=lesrecettesdudeveloppeur", "root", "");
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * Permet de récupérer un enregistrement dans la base de données.
+     *
+     * @param string $sql
+     * @param array $params
+     * @return array|false
+     */
+    public function selectUneDonne(string $sql, array $params = [])
+    {
+        $requete = $this->db->prepare($sql);
+        $requete->execute($params);
+
+        return $requete->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Permet de récupérer plusieurs enregistrements dans la base de données.
+     *
+     * @param string $sql
+     * @param array $params
+     * @return array
+     */
+    public function selectTouteDonne(string $sql, array $params = [])
+    {
+        $requete = $this->db->prepare($sql);
+        $requete->execute($params);
+
+        return $requete->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Permet de retouner le dernier id
+     *
+     * @return string
+     */
+    public function dernierIdInsere()
+    {
+        return $this->db->lastInsertId();
+    }
+}
+
+
+?>
